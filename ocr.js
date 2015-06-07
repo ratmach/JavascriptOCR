739225d0969ad0e28e2ce1dcb14a00da149dce7a
ratmach <ratmach@yahoo.com>
2015-06-07 22:11:39 +0400

    Started working on JS part
    ok this is probably how it's done

---
+++ ocr.js
@@ -0,0 +1,9 @@
+var numbers={[],[],[],[],[],[],[],[],[],[]};
+function checkCanvas(canvas,height,width){
+	var cont = canvas.getContext("2d").getImageData(0,0,height,width);
+	for(var i=0;i<height;++i){
+		for(var j=0;j<width;++j){
+			
+		}
+	}
+}
