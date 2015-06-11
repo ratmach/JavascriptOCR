38575b0dae41940bd27c1b242b7caeaac2024982
ratmach <ratmach@yahoo.com>
2015-06-11 22:41:13 +0400

    added downscale

--- ocr.js
+++ ocr.js
@@ -1,24 +1,24 @@
 var numbers={[],[],[],[],[],[],[],[],[],[]};
 function checkCanvas(canvas,height,width){
 	var cont = canvas.getContext("2d").getImageData(0,0,height,width);
-	for(var i=0;i<height;++i){
-		for(var j=0;j<width;++j){
+	cont = rescale(cont,height,width);
+}
+/**
+* downscales image first
+*breaks image down into pieces so that multiple symbols can be read
+*prototype of an algorithm will work in o(n^2) precisely o((n^2)*number of not connected figures *2)
+**/
+function rescale(cont,height,width){
+	var downscale=new Array([]);
+	var cont = canvas.getContext("2d").getImageData(0,0,height,width);
+	var x1=null;//find offset of rectangle for part of the picture
+	var y1=null;
+	var x2=null;
+	var y2=null;
+	for(var y=0;y<height;++y){
+		for(var x=0;x<width;++x){
 			
 		}
 	}
-}
-function rescale(){
-	var cont = canvas.getContext("2d").getImageData(0,0,height,width);
-	var x=null;
-	var y=null;
-	for(var y=0;y<height;++y){
-		for(var x=0;x<width;++x){
-			if(cont[x][y]==255){
-				
-			}
-		}
-	}
-}
-function findX(cont,height,width){
 	
 }
